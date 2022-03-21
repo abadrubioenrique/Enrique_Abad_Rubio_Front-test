0342779a1cc418cc83131b5f4f38a68bdf257848
abadrubioenrique <abadrubioenrique@gmail.com>
2022-03-21 15:35:29 +0100

    Sass y HeaderComponent
    Sass incluido, Header: Logo y  search input responsivos

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Headercomponent from "./components/headerComponent";
+
+function App() {
+  return (
+
+    <Headercomponent></Headercomponent>
+
+  );
+}
+
+export default App;
---
+++ src/components/headerComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../img/logo.png';
+import '../styles/header.scss';
+
+import {FiSearch} from 'react-icons/fi'
+const Headercomponent = () => {
+    return (
+        <div className='nav'>
+            <div className="nav__logo">
+            <img src={logo} alt="Logo"/>
+            </div>
+            <div className="nav__search">
+            <div className="nav__search__icon">
+            <FiSearch/>
+            </div>
+            
+                <input type='text' placeholder="You're looking for something?"></input>
+            </div>
+        </div>
+
+    );
+}
+
+export default Headercomponent;
